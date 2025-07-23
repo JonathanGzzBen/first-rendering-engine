52a39081d271ce1bf5750409f31f67412ae118c0
JonathanGzzBen <jonathangzzben@gmail.com>
2025-07-23 09:59:47 -0600

    Add debugging context and callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,31 @@
 
 #include "frengine/renderer.h"
 
+auto glfw_error_callback(int error, const char *description) -> void {
+  std::println(std::cerr, "GLFW error {}: {}", error, description);
+}
+
+void APIENTRY glDebugCallback(GLenum source, GLenum type, GLuint id,
+                              GLenum severity, GLsizei length,
+                              const GLchar *message, const void *userParam) {
+  std::println(std::cerr, "glDebugCallback: {}: {}", type, message);
+}
+
 auto main() -> int {
   if (glfwInit() != GLFW_TRUE) {
     std::println(std::cerr, "Could not initialize glfw");
     return 1;
   }
 
-  GLFWwindow* window =
+  glfwSetErrorCallback(glfw_error_callback);
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+  GLFWwindow *window =
       glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
   if (window == nullptr) {
     std::println(std::cerr, "Could not create GLFW window");
@@ -27,6 +45,19 @@ auto main() -> int {
     glfwTerminate();
     return 1;
   }
+
+  // Configure OpenGL debugging if Debug context was created
+  []() {
+    int context_flags;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &context_flags);
+    if (context_flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+      glEnable(GL_DEBUG_OUTPUT);
+      glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+      glDebugMessageCallback(glDebugCallback, nullptr);
+      glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0,
+                            nullptr, GL_TRUE);
+    }
+  }();
 
   std::println("{}", frengine::Renderer::GetHelloWorld());
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
