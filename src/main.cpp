ce038ccc3040335c13b9e5d9c4f6a5ef3156ecd1
JonathanGzzBen <jonathangzzben@gmail.com>
2025-07-23 09:37:15 -0600

    Add initial glfw+glew window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 #include <print>
 
 #include "frengine/renderer.h"
 
 auto main() -> int {
+  if (glfwInit() != GLFW_TRUE) {
+    std::println(std::cerr, "Could not initialize glfw");
+    return 1;
+  }
+
+  GLFWwindow* window =
+      glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+  if (window == nullptr) {
+    std::println(std::cerr, "Could not create GLFW window");
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK) {
+    std::println(std::cerr, "Could not initialize GLEW");
+    glfwTerminate();
+    return 1;
+  }
+
   std::println("{}", frengine::Renderer::GetHelloWorld());
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
   return 0;
 }
